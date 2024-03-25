7718b38bd6c41d7bde2b1102e77cda9e346cfcc4
Ignacio Lencina <nacholencina60@gmail.com>
2024-03-25 20:23:48 -0300

    Resuelvo el ejercicio numero 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,6 @@
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+let nombreUsuario = prompt("Escriba su nombre de usuario");
+console.log("Hola " + nombreUsuario);
+
+
