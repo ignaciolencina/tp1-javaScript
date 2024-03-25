2dbb78b87e176b80a64950fd991caead8110832f
Ignacio Lencina <nacholencina60@gmail.com>
2024-03-25 20:35:10 -0300

    Resuelvo el ejercicio numero 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,5 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+let num1 = parseInt(prompt("Introduce el primer número"));
+let num2 = parseInt(prompt("Introduce el segundo número"));
+alert("El resultado de la suma de estos dos números es = " + (num1 + num2));
