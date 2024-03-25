fccd0b8716ea88ba9e8ac8b99502de9a23947914
Ignacio Lencina <nacholencina60@gmail.com>
2024-03-25 19:59:39 -0300

    Primer commit de los ejercicios de Js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+console.log("Hola mundo!");
+console.log("Hola mundo mundial!");
+
+let nombre = "Nacho";
+let miNumero = 6;
+
+console.log(nombre);
+console.log(miNumero);
+
+miNumero = "TWD";
+
+console.log(miNumero);
+
+// alert("Este es un texto en el alert, bienvenido " + nombre);
+
+// const respuesta = prompt("Ingrese su nombre");
+// console.log(respuesta);
+
+// console.log(2 > 5);
+// console.log(2 < 5);
+// console.log(2 >= 5);
+// console.log(2 <= 5);
+// console.log(5 > 5);
+// console.log(5 >= 5);
+// console.log("5" == 5);
+// console.log(5 === "5");
+// console.log(5 != "5");
+// console.log(5 !== "5");
+
+// Si esto se cumple (es true)
+if (2 > 10) {
+  console.log("Se ejecutó esto");
+} else if (15 === 10) {
+    console.log("Se ejecuta esto otro")
+} else {
+    console.log("Se ejecuta esto último")
+}
+
+document.write("Hola")
+
